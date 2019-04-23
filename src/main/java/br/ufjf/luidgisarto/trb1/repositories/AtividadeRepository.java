383e4daee10802a24bf38d65be699dee7509bea1
Luidgi Sarto Lacerda <luidgi.lacerda@gmail.com>
2019-04-23 07:39:24 -0300

    Criação do repository de atividade

---
+++ src/main/java/br/ufjf/luidgisarto/trb1/repositories/AtividadeRepository.java
@@ -0,0 +1,9 @@
+package br.ufjf.luidgisarto.trb1.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.ufjf.luidgisarto.trb1.models.Atividade;
+
+public interface AtividadeRepository extends JpaRepository<Atividade, Long> {
+
+}
