c9aa1953eb151af4bb24990a6d2edcf688ccb48d
Luidgi Sarto Lacerda <luidgi.lacerda@gmail.com>
2019-04-23 07:39:50 -0300

    Criação da model de Membro

---
+++ src/main/java/br/ufjf/luidgisarto/trb1/models/Membro.java
@@ -0,0 +1,141 @@
+package br.ufjf.luidgisarto.trb1.models;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+/**
+ * Membro
+ */
+@Entity
+public class Membro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String nomeCompleto;
+    private String funcao;
+    private String email;
+    private Date dataEntradaFuncao;
+    private Date dataSaidaFuncao;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "sedeId", nullable = false)
+    private Sede sede;
+
+    public Membro() {
+
+    }
+
+    public Membro(String nome, String funcao, String email, Date dataEntrada, Date dataSaida, Sede sede) {
+        this.nomeCompleto = nome;
+        this.funcao = funcao;
+        this.email = email;
+        this.dataEntradaFuncao = dataEntrada;
+        this.dataSaidaFuncao = dataSaida;
+        this.sede = sede;
+    }
+
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nomeCompleto
+     */
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    /**
+     * @param nomeCompleto the nomeCompleto to set
+     */
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    /**
+     * @return the funcao
+     */
+    public String getFuncao() {
+        return funcao;
+    }
+
+    /**
+     * @param funcao the funcao to set
+     */
+    public void setFuncao(String funcao) {
+        this.funcao = funcao;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the dataEntradaFuncao
+     */
+    public Date getDataEntradaFuncao() {
+        return dataEntradaFuncao;
+    }
+
+    /**
+     * @param dataEntradaFuncao the dataEntradaFuncao to set
+     */
+    public void setDataEntradaFuncao(Date dataEntradaFuncao) {
+        this.dataEntradaFuncao = dataEntradaFuncao;
+    }
+
+    /**
+     * @return the dataSaidaFuncao
+     */
+    public Date getDataSaidaFuncao() {
+        return dataSaidaFuncao;
+    }
+
+    /**
+     * @param dataSaidaFuncao the dataSaidaFuncao to set
+     */
+    public void setDataSaidaFuncao(Date dataSaidaFuncao) {
+        this.dataSaidaFuncao = dataSaidaFuncao;
+    }
+
+    /**
+     * @return the sede
+     */
+    public Sede getSede() {
+        return sede;
+    }
+
+    /**
+     * @param sede the sede to set
+     */
+    public void setSede(Sede sede) {
+        this.sede = sede;
+    }
+}
