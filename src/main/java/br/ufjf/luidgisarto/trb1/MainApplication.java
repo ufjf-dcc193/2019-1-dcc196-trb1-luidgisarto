b574c8e4aef83dbc8bc7d93c9597d7729b09ec43
Luidgi Sarto Lacerda <luidgi.lacerda@gmail.com>
2019-04-23 07:40:25 -0300

    Carregamento Inicial de dados

--- src/main/java/br/ufjf/luidgisarto/trb1/MainApplication.java
+++ src/main/java/br/ufjf/luidgisarto/trb1/MainApplication.java
@@ -1,12 +1,40 @@
 package br.ufjf.luidgisarto.trb1;
 
+import java.util.Date;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import br.ufjf.luidgisarto.trb1.models.Atividade;
+import br.ufjf.luidgisarto.trb1.models.Membro;
+import br.ufjf.luidgisarto.trb1.models.Sede;
+import br.ufjf.luidgisarto.trb1.repositories.AtividadeRepository;
+import br.ufjf.luidgisarto.trb1.repositories.MembroRepository;
+import br.ufjf.luidgisarto.trb1.repositories.SedeRepository;
 
 @SpringBootApplication
 public class MainApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(MainApplication.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(MainApplication.class, args);
+
+		SedeRepository sedeRep = ctx.getBean(SedeRepository.class);
+		MembroRepository membroRep = ctx.getBean(MembroRepository.class);
+		AtividadeRepository atividadeRep = ctx.getBean(AtividadeRepository.class);
+
+		for (int i = 1; i < 4; i++) {
+			Sede s = new Sede("Sede" + (i), "MG", "Juiz de Fora", "Centro", "32 99999-9999", "sede" + i + ".com.br");
+
+			Sede sede = sedeRep.save(s);
+
+			Membro m = new Membro("Membro" + i, "Developer", "developer@gmail.com", new Date(), new Date(), sede);
+
+			Atividade a = new Atividade("Atividade" + i, "Descrição " + i, new Date(), new Date(), 10, 20, 10, 20, s);
+
+			atividadeRep.save(a);
+			membroRep.save(m);
+
+		}
 	}
 
 }
